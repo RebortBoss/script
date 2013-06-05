6f7e59219e3be448d9e20a369f178d0c0f12409a
zhoudd <zdd9999@gmail.com>
2013-06-05 23:12:26 +0800

    up 修改

--- tms/exportData/export_bushuji.sh
+++ tms/exportData/export_bushuji.sh
@@ -4,18 +4,19 @@ if [ -z "$1" ];then
   echo "lastDate is null"
   exit 0;
 fi;
-if [ -z "$2" ];then
-  echo "not part_table_name"
-  exit 0;
-fi;
-printf "TSP-1:当前取到的日期是:$1,下一分区表为:$2,是否继续(y/n)："
+#if [ -z "$2" ];then
+#  echo "not part_table_name"
+#  exit 0;
+#fi;
+#printf "TSP-1:当前取到的日期是:$1,下一分区表为:$2,是否继续(y/n)："
+printf "TSP-1:当前取到的日期是: $1 ,是否继续(y/n)："
 read -r passFlag
 echo $passFlag
 if [ ! "$passFlag" = "y" ]; then
   echo "退出...."
   exit;
 fi;
-export ORACLE_BASE=/home/zhoudd/setup/oracle/oracle
+export ORACLE_BASE=/oracle/app/oracle
 export ORACLE_HOME=$ORACLE_BASE/11.2.0
 export LD_LIBRARY_PATH=$ORACLE_HOME/lib
 export PATH=$ORACLE_HOME/bin:/usr/local/sbin:/usr/local/bin:$PATH
@@ -37,23 +38,33 @@ select h_part('TMS_MAIL_MONITOR_INFO',TO_DATE('$1','yyyymmddhh24miss')+1/12) h_p
 EOF
 `
 echo $calcPtName
-if [ "$calcPtName" != "$2" ];then
-  printf "TSP-2:计算出来的分区表名与输入不下一致,输入:$2　查出来:$calcPtName ,是否继续(y/n)："
-  read -r pqc
-  echo $pqc
-  if [ ! "$pqc" = "y" ]; then
-   echo "退出...."
+
+#if [ "$calcPtName" != "$2" ];then
+#  printf "TSP-2:计算出来的分区表名与输入不下一致,输入:$2　查出来:$calcPtName ,是否继续(y/n)："
+#  read -r pqc
+#  echo $pqc
+#  if [ ! "$pqc" = "y" ]; then
+#   echo "退出...."
+#   exit 0;
+#  fi;
+#fi;
+if [ -z "$calcPtName" ];then
+  printf "TSP-3:计算不出分区来,是否继续(y/n)："
+  read -r pdt
+  if [ ! "$pdt" = "y" ]; then
+   echo "TSP-2:退出...."
    exit 0;
   fi;
 fi;
+
 
 sqlplus -S $LogIn <<SEF
 set timing on;
 set time on;
 set show on;
 set echo on;
-select sysdate,'$1' p1,'$2' p2 from dual;
-select h_part('TMS_MAIL_MONITOR_INFO',TO_DATE('$1','yyyymmddhh24miss')+1/12) h_part_name,'$2' parname from dual;
+select sysdate,'$1' p1,'$calcPtName' p2 from dual;
+select h_part('TMS_MAIL_MONITOR_INFO',TO_DATE('$1','yyyymmddhh24miss')+1/12) h_part_name,'$calcPtName' parname from dual;
 
 drop table LEE_TEMP_5 purge;
 
@@ -68,10 +79,34 @@ drop table LEE_TEMP_1 purge;
 create table LEE_TEMP_1 
 nologging parallel 16 
 as
-select * from TMS_MAIL_MONITOR_INFO partition($2)
+select nm.mail_no,nm.ACTION_TYPE,
+  nm.plan_agency,
+  nm.PLAN_FREQUENCY,
+  nm.ACTUAL_AGENCY,
+  nm.ACTUAL_FREQUENCY,
+  nm.plan_action_time,nm.actual_action_time,
+  nm.TRIP_FLIGHTS_CODE,nm.ROUTE_CODE,nm.ACTION_PRESON, nm.NEXT_ORGCODE,
+  nm.NEXT_FRE,
+  nm.NEXT_ORG_TIME,
+  NM.ROUTE_NAME,NM.TRIP_FLIGHTS_NAME, NM.UNUSUAL_RESON, NM.MAIL_BAG_LABEL,NM.PKG_BUSI_KIND_CODE, 
+  nm.DLV_STS_CODE,nm.UNDLV_NEXT_ACTN_CODE,
+  nm.CREATE_TIME, nm.DIRECTION, nm.SEAL_ORG_CODE,nm.RCV_ORG_CODE, 
+  NM.UPDATE_TIME from TMS_MAIL_MONITOR_INFO partition($calcPtName) nm
 where ACTUAL_ACTION_TIME is not null;
 
-insert /* +append parallel(t 16)*/ into LEE_TEMP_1  select  * from LEE_TEMP_5 t;
+insert /* +append parallel(nm 16)*/ into LEE_TEMP_1  select  nm.mail_no,nm.ACTION_TYPE,
+  nm.plan_agency,
+  nm.PLAN_FREQUENCY,
+  nm.ACTUAL_AGENCY,
+  nm.ACTUAL_FREQUENCY,
+  nm.plan_action_time,nm.actual_action_time,
+  nm.TRIP_FLIGHTS_CODE,nm.ROUTE_CODE,nm.ACTION_PRESON, nm.NEXT_ORGCODE,
+  nm.NEXT_FRE,
+  nm.NEXT_ORG_TIME,
+  NM.ROUTE_NAME,NM.TRIP_FLIGHTS_NAME, NM.UNUSUAL_RESON, NM.MAIL_BAG_LABEL,NM.PKG_BUSI_KIND_CODE, 
+  nm.DLV_STS_CODE,nm.UNDLV_NEXT_ACTN_CODE,
+  nm.CREATE_TIME, nm.DIRECTION, nm.SEAL_ORG_CODE,nm.RCV_ORG_CODE, 
+  NM.UPDATE_TIME  from LEE_TEMP_5 nm;
 
 commit;
 
