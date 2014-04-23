5def1a9cf052af5c4a20d4a9a854e963ada740ec
zhoudd <zdd9999@gmail.com>
2014-04-23 14:09:45 +0800

    删除部分信息

--- python/send_capture_data.sh
+++ python/send_capture_data.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 file_name="业务量数据记录-`date -d '1 days ago' +'%Y-%m-%d'`.txt"
 echo "$file_name"
-cat /opt/test/$file_name | /usr/local/bin/mutt -s "$file_name" zhoudd@ems.com.cn,wangys@hollycrm.com,lichao@hollycrm.com
+cat /opt/test/$file_name | /usr/local/bin/mutt -s "$file_name" xxx@xxx.com.cn,xxx@xxx.com,xxx@xxx.com
